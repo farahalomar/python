6ed23853d9d9282b255689a84721983695731cc4
Farah AlOmar <farahjalomar@gmail.com>
2019-08-06 13:47:07 +0300

    String task solution

---
+++ string_task.py
@@ -0,0 +1,7 @@
+time = input("Enter a time ")
+items = input("Enter an item you like ")
+name = input("Enter a name ").capitalize()
+scream = input("Enter a random sentence ").upper()
+action = input("Enter an action you would do when scared ")
+
+print("It was", time, "when I heard a knock at the door", ".", "\nI opened the door and there was", items, "with a note saying from", name, ".", "\nJust as I closed the door I heard a scream", scream, ".", "\nI froze in place and all I could do was", action, "."
